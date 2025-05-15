1d31b41da74ca2864259ec3bbfb6f02b2ca77d4b
AnatoliyUrev86 <alexurev75@gmail.com>
2025-05-15 11:03:32 +0300

    commit

--- js/task-1.js
+++ js/task-1.js
@@ -1,2 +1,12 @@
-const itemEl = document.querySelectorAll(".item-list");
-console.log(itemEl.children);
+const listEl = document.querySelectorAll(".item-list");
+console.log(listEl);
+
+const itemEl = document.querySelectorAll(".item");
+console.log(itemEl);
+
+// const titleEl = document.querySelectorAll(".title");
+// console.log(titleEl);
+
+const titleEl = document.querySelectorAll(".title");
+titleEl.textContent = "Animals";
+console.log(titleEl);
