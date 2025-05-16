3146e6f100ac4fa0c2dd5424eacb7d57b2b38a93
AnatoliyUrev86 <alexurev75@gmail.com>
2025-05-16 17:42:50 +0300

    commit

--- js/task-4.js
+++ js/task-4.js
@@ -4,5 +4,7 @@ formEl.addEventListener("submit", (event) => {
   event.currentTarget.elements;
   console.log(event.currentTarget.elements);
   const inputEmail = event.currentTarget.elements.email.value.trim();
+  console.log(inputEmail);
   const inputPasword = event.currentTarget.elements.password.value.trim();
+  console.log(inputPasword);
 });
