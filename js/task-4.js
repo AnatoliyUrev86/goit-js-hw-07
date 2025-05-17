f5e1e5b9617513fabe50ff72e9d3d50d3eea929b
AnatoliyUrev86 <alexurev75@gmail.com>
2025-05-17 09:36:08 +0300

    commit

--- js/task-4.js
+++ js/task-4.js
@@ -7,4 +7,27 @@ formEl.addEventListener("submit", (event) => {
   console.log(inputEmail);
   const inputPasword = event.currentTarget.elements.password.value.trim();
   console.log(inputPasword);
+  const userName = document.querySelector(".js-user-mane");
+  userName.addEventListener("input", (event) => {
+    console.log(event.target.value);
+  });
+  const form = document.querySelector(".login-form");
+  form.addEventListener("submit", handleSubmit);
+  function handleSubmit(event) {
+    console.log(event.currentTarget.elements);
+    event.target.reset();
+  }
+  event.preventDefault();
+  const info = {
+    email: elements.email.value,
+    password: elements.password.value,
+  };
+  const btn = document.querySelector(".js-click");
+  click.addEventListener("change", () => {});
+  userName.addEventListener("blur", handBlur);
+  function handBlur(event) {
+    const name = event.target.value;
+    alert(`'All form fields must be filled in' ${name}`);
+  }
+  console.log(info);
 });
