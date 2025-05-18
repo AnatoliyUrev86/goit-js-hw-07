5703a431bd4141285fe470c4b26a8096b72f3e0f
AnatoliyUrev86 <alexurev75@gmail.com>
2025-05-18 09:39:13 +0300

    commit

--- js/task-5.js
+++ js/task-5.js
@@ -1,7 +1,9 @@
+const colorPickerContainerEl = document.querySelector(".widget");
+const btnEl = document.querySelector(".change-color");
+btnEl.addEventListener("change", getRandomHexColor);
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
-
-const buttonEl = document.querySelector(".change-color");
