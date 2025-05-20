1b79dbf1491b3f1c17a3eb0bc72ba920bc9e31f8
AnatoliyUrev86 <alexurev75@gmail.com>
2025-05-20 11:23:26 +0300

    commit

--- js/task-5.js
+++ js/task-5.js
@@ -1,8 +1,9 @@
 const colorPickerContainerEl = document.querySelector(".color-picker");
 const btnEl = document.querySelector(".change-color");
 btnEl.addEventListener("click", getRandomHexColor);
-
-function getRandomHexColor() {
+document.body.style.backgroundColor = "#fafafa;";
+function getRandomHexColor(event) {
+  console.log(event);
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
